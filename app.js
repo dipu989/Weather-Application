8aeacc011dea8e22339933641c95ebe4acd377da
dipu989 <me.shantnu@gmail.com>
2018-10-15 15:43:05 +0530

    Add remaining functionalities.

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var humidity;
 var wind;
 var direction;
 
-function updateByPin(pin,co)
+function updateByPin(pin)
 {
   var url = "http://api.openweathermap.org/data/2.5/weather?"+
   "zip="+ pin +
   "&APPID=" + APPID;
-  console.log(url);
   sendRequest(url);
 }
 
-function updateByGeo(lat, lon){
+function updateByGeo(lat, lon)
+{
   var url = "http://api.openweathermap.org/data/2.5/weather?"+
   "lat=" + lat +
   "&lon=" + lon +
@@ -23,54 +23,68 @@ function updateByGeo(lat, lon){
   sendRequest(url);
 }
 
+function showPosition(position)
+{
+  updateByGeo(position.coords.latitude,position.coords.longitude);
+}
+
+function KelvinToCelcius(temp)
+{
+  return Math.round(temp-273.5);
+}
+
 function sendRequest(url)
 {
   var xmlhttp = new XMLHttpRequest();
+
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       var data = JSON.parse(xmlhttp.responseText);
+      console.log(data.weather[0].description);
       var weather = {};
       weather.icon = data.weather[0].id;
       weather.humidity = data.main.humidity;
       weather.wind = data.wind.speed;
-      weather.direction = data.wind.deg;
       weather.loc = data.name;
-      weather.temp = data.main.temp;
-      xmlhttp.open("GET",url,true);
-      xmlhttp.send();
+      weather.temp = KelvinToCelcius(data.main.temp);
+      weather.description = data.weather[0].description;
+      update(weather);
   }
 };
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
 }
 
-function showPosition(position){
-  updateByGeo(position.coords.latitude,position.coords.longitude);
-}
-
-function update(weather) {
+function update(weather)
+{
   wind.innerHTML = weather.wind;
-  direction.innerHTML = weather.direction;
   humidity.innerHTML = weather.humidity;
   loc.innerHTML = weather.loc;
   temp.innerHTML = weather.temp;
   icon.src = "imgs/codes/" + weather.icon + ".png";
+  description.innerHTML = weather.description;
 }
 
-window.onload = function() {
+window.onload = function()
+{
   temp = document.getElementById("temp");
   loc = document.getElementById("loc");
   icon = document.getElementById("icon");
   humidity = document.getElementById("humidity");
   wind = document.getElementById("wind");
-  direction = document.getElementById("direction");
+  description = document.getElementById("description");
 
-  var pos = navigator.geolocation.getCurrentPosition(showPosition);
-
-if(navigator.geolocation){
-    var pos = navigator.geolocation.getCurrentPosition(showPosition);
+if(navigator.geolocation)
+{
+    var showPosition = function(position)
+    {
+      updateByGeo(position.coords.latitude,position.coords.longitude);
+    }
+      navigator.geolocation.getCurrentPosition(showPosition);
   }
-  else{
+  else
+  {
     var pin = window.prompt("We could not discover your location. What is your zip Code?");
     updateByPin(pin);
   }
-  updateByPin(700102);
 }
